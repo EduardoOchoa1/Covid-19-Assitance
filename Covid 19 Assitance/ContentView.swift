79279a83999f5177e7ab6e892f7a971294577a29
Eduardo O <eduxone@icloud.com>
2021-12-07 16:25:00 -0600

    Initial Commit

---
+++ Covid 19 Assitance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid 19 Assitance
+//
+//  Created by Central States SER Mac 18 on 12/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid 19 Assitance/Covid_19_AssitanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_19_AssitanceApp.swift
+//  Covid 19 Assitance
+//
+//  Created by Central States SER Mac 18 on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Covid_19_AssitanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
