aeebaa7adc3d5955de17d65eadf02bc0ba37e944
Eduardo O <eduxone@icloud.com>
2021-12-08 15:43:34 -0600

    Added Map

--- Covid 19 Assitance/ContentView.swift
+++ Covid 19 Assitance/ContentView.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State var input: String = ""
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            
+            HStack{
+                TextField("Search:", text: $input)
+                Button(action:{}, label: {})
+            }
             .padding()
+            MapView()
+        }
     }
+}
+
+struct MapView: UIViewRepresentable {
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+    
+    func makeUIView(context: Context) -> MKMapView{
+        let map = MKMapView()
+        map.mapType = .standard
+        map.isRotateEnabled = false
+        
+        let location = CLLocation(latitude: 41.8781, longitude: -87.6298)
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        map.setRegion(region, animated: false)
+        return map
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
